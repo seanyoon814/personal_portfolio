8daec40ee825defd5e80a79b2662b862063136fa
Sean Yoon <shy13@sfu.ca>
2025-04-15 19:57:55 -0700

    changed dependencies, added title and logo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,7 +31,10 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en-GB'>
         <Head>
+          <title>Sean Yoon | Portfolio</title>
           <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
+          <link rel="icon" href="images/watermelon.png" />
+
         </Head>
         <body>
           <Main />
